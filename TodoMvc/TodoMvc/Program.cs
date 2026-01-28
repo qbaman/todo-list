92ef7a4753ed0f42cde221949764f12b677754b9
qbaman <78668489+qbaman@users.noreply.github.com>
2026-01-28 10:20:16 +0000

    Commit: Register CSV import service
    Registered CsvImportService with dependency injection so controllers can import tasks from CSV files.

--- TodoMvc/TodoMvc/Program.cs
+++ TodoMvc/TodoMvc/Program.cs
@@ -1,7 +1,10 @@
+using TodoMvc.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddTransient<CsvImportService>();
 
 var app = builder.Build();
 
--- TodoMvc/TodoMvc/obj/Debug/net9.0/TodoMvc.AssemblyInfo.cs
+++ TodoMvc/TodoMvc/obj/Debug/net9.0/TodoMvc.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TodoMvc")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4860b39ebc5d9cde1c2c9e0878b22681c3aa53b2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dca798606261476845876f39adf8a246028c911d")]
 [assembly: System.Reflection.AssemblyProductAttribute("TodoMvc")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TodoMvc")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
